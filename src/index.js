dfd300095510c81dab9fa96be9f3ac2fa0407411
beenar-0 <dncastledn@gmail.com>
2022-05-09 02:44:34 +0300

    feat: add keyboard template

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import render from "./utils/render"
 import key from "./utils/key"
 import langs from "./langs/langs";
+import Keyboard from './utils/keyboard'
 import './style.css'
 
-// render(elem, classList, child = null, parent = null, ...attributes)
+// render(elem, [classList], [child], parent = null, ...attributes)
 
+const keyboard = new Keyboard('eng')
+keyboard.build()
