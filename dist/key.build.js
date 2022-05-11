4391ac3f3051fd0f6381fffd75f1e361a7092f6f
Nikita Dubovik <68840670+beenar-0@users.noreply.github.com>
2022-05-12 02:45:55 +0300

    Add files via upload

--- dist/key.build.js
+++ dist/key.build.js
@@ -16,14 +16,45 @@
   \**************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ Key)\n/* harmony export */ });\nclass Key {\r\n    constructor(low, up, code) {\r\n        this.code = code\r\n        this.low = low\r\n        this.up = up\r\n    }\r\n    create(){\r\n        return 'aboba'\r\n    }\r\n}\n\n//# sourceURL=webpack://virtual-keyboard/./src/utils/key.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ Key)\n/* harmony export */ });\n/* harmony import */ var _render__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./render */ \"./src/utils/render.js\");\n\r\n\r\nclass Key {\r\n    constructor(low, up, code) {\r\n        this.code = code\r\n        this.low = low\r\n        if (up === 'null') this.up = low\r\n        else this.up = up\r\n        this.isCapsable = !code.match(/^Key[A-Z]$/);\r\n        this.isFunc = !code.match(/^Key[A-Z]|BracketLeft|Backquote|Slash|Period|Comma|Quote|Semicolon|Backslash|BracketRight$/);\r\n    }\r\n\r\n    setActive(){\r\n\r\n    }\r\n\r\n}\r\n\n\n//# sourceURL=webpack://virtual-keyboard/./src/utils/key.js?");
+
+/***/ }),
+
+/***/ "./src/utils/render.js":
+/*!*****************************!*\
+  !*** ./src/utils/render.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ render)\n/* harmony export */ });\nfunction render(elem, classList, child, parent, ...attributes) {\n  const element = document.createElement(elem);\n  classList.forEach((item) => {\n    element.classList.add(item);\n  });\n  if (typeof child === 'object' && child !== null) {\n    child.forEach((item) => {\n      element.appendChild(item);\n    });\n  } else {\n    element.innerHTML = child;\n  }\n  if (parent) parent.appendChild(element);\n  attributes.forEach((item) => {\n    if (item[0].match(/id|value|placeholder|cols|rows|autocorrect|spellcheck/)) {\n      element.setAttribute(item[0], item[1]);\n    } else {\n      [element.dataset[item[0]]] = item;\n    }\n  });\n  return element;\n}\n\n\n//# sourceURL=webpack://virtual-keyboard/./src/utils/render.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/define property getters */
@@ -59,8 +90,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/utils/key.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/utils/key.js");
 /******/ 	
 /******/ })()
 ;
