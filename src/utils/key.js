afab6e99c75e722e196998125eccc5e46091481c
beenar-0 <dncastledn@gmail.com>
2022-05-12 20:12:10 +0300

    refactor: add eslint

--- src/utils/key.js
+++ src/utils/key.js
@@ -1,13 +1,11 @@
-import render from "./render";
-
 export default class Key {
-    constructor(low, up, code) {
-        this.code = code
-        this.low = low
-        if (up === 'null') this.up = low
-        else this.up = up
-        this.isCapsable = !code.match(/^Key[A-Z\d]$/);
-        this.isFunc = !code.match(/^Key[A-Z]|BracketLeft|Backquote|Slash|Period|Comma|Quote|Semicolon|Backslash|BracketRight|^Digit\d|Equal|Minus$/);
-        if (this.low.match(/^[а-я]$/)) this.isCapsable = false
-    }
+  constructor(low, up, code) {
+    this.code = code;
+    this.low = low;
+    if (up === 'null') this.up = low;
+    else this.up = up;
+    this.isCapsable = !code.match(/^Key[A-Z\d]$/);
+    this.isFunc = !code.match(/^Key[A-Z]|BracketLeft|Backquote|Slash|Period|Comma|Quote|Semicolon|Backslash|BracketRight|^Digit\d|Equal|Minus$/);
+    if (this.low.match(/^[а-я]$/)) this.isCapsable = false;
+  }
 }
